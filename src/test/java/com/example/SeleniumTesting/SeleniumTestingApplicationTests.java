cfbc6d8c9405b52fc6234fd0872a2b8c0a67dcbf
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2023-12-27 17:06:01 -0300

    first commit

---
+++ src/test/java/com/example/SeleniumTesting/SeleniumTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SeleniumTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
